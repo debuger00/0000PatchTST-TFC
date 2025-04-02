e5e11dafc5e0fb5051660b0ccd711ab0884f9340
debuger00 <wyh3209379906@foxmail.com>
2025-04-02 09:57:39 +0800

    本地同步

--- CMI-Net/training_script_wyh.sh
+++ CMI-Net/training_script_wyh.sh
@@ -17,21 +17,15 @@ echo "Allocate Gpu Units:"$CUDA_VISIBLE_DEVICES
 
 nvidia-smi
 
-# 定义时间变量
 current_time=$(date "+%Y-%m-%d_%H_%M_%S")
 echo "当前时间是: $current_time"
 
-# 定义基础保存路径
-base_path="results/${current_time}"
-mkdir -p $base_path
 
-# 运行每个fold的训练
-python train5_ConvTrans_auto.py  --data_path './data/000goat.pt' --save_path "${base_path}/setting000"
-python train5_ConvTrans_auto.py  --data_path './data/001goat.pt' --save_path "${base_path}/setting001"
-python train5_ConvTrans_auto.py  --data_path './data/002goat.pt' --save_path "${base_path}/setting002"
-python train5_ConvTrans_auto.py  --data_path './data/003goat.pt' --save_path "${base_path}/setting003"
-python train5_ConvTrans_auto.py  --data_path './data/004goat.pt' --save_path "${base_path}/setting004"
-python train5_ConvTrans_auto.py  --data_path './data/005goat.pt' --save_path "${base_path}/setting005"
+python train5_ConvTrans_auto.py  --data_path './data/000goat.pt' --save_path "setting000/${current_time}"
+python train5_ConvTrans_auto.py  --data_path './data/001goat.pt' --save_path "setting001/${current_time}"
+python train5_ConvTrans_auto.py  --data_path './data/002goat.pt' --save_path "setting002/${current_time}"
+python train5_ConvTrans_auto.py  --data_path './data/003goat.pt' --save_path "setting003/${current_time}"
+python train5_ConvTrans_auto.py  --data_path './data/004goat.pt' --save_path "setting004/${current_time}"
+python train5_ConvTrans_auto.py  --data_path './data/005goat.pt' --save_path "setting005/${current_time}"
 
-# 运行汇总脚本，保存在同一个时间文件夹下
-python 五折汇总.py --save_path "${base_path}/summary"
+python 五折汇总.py --save_path "setting999${current_time}"
